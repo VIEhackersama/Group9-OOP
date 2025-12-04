c09bd8cb55ee2e073867352151bdcf63cf23d450
huyle-uii <huyle4549@gmail.com>
2025-12-04 11:05:03 +0700

    1

--- src/main/java/org/yourcompany/yourproject/Interface/RealEstate.java
+++ src/main/java/org/yourcompany/yourproject/Interface/RealEstate.java
@@ -1,7 +1,7 @@
 package org.yourcompany.yourproject.Interface;
 
 public interface RealEstate {
-    public static final city = "Hanoi";
+    
     
     public double predictPrice() throws Exception;
 }
